a88e94e84c3321eb5727bc60dd8a292108d937aa
Sam Haveson <sehaveson@gmail.com>
2025-02-24 17:36:07 -0500

    Add new musical note favicon and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
